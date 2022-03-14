2938947e5577acfab7cdcff396ece03c1817ac5d
MiguelsPizza <alexmnahas@gmail.com>
2022-03-14 18:01:43 -0400

    added firebase and login page

--- client/App.jsx
+++ client/App.jsx
@@ -1,57 +1,118 @@
-import React, { useState, useEffect } from "react";
-import MainB from "./RSSI/MainB.jsx";
-import TopBar from "./RSSI/TopBar.jsx";
-import SideBar from "./RSSI/SideBar/SideBar.jsx";
+import React from "react";
+import MainPage from "./MainPage.jsx";
+// import SignIn from "./SignIn.jsx"
+import firebase from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA6V10s47nKkBakTxMupMXA4VaRyIpI0Xs",
+  authDomain: "findfi-15087.firebaseapp.com",
+  projectId: "findfi-15087",
+  storageBucket: "findfi-15087.appspot.com",
+  messagingSenderId: "418885153057",
+  appId: "1:418885153057:web:1d6a636b046e3047910e60",
+  measurementId: "G-E8PC4ZWM9X",
+};
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+const firestore = getFirestore();
 
 const App = () => {
-  const [rssi, changeRssi] = useState(null);
-  const [toggle, changeToggle] = useState(false);
+  const [user] = useAuthState(auth);
 
-
-  const upDateWIFIdata = (data) =>{
-    console.log(data[0])
-    changeRssi(data[0].signal_level)
-  }
-  useEffect(() => {
-    if (toggle) {
-      let eventSource = new EventSource("http://localhost:3000/rssi");
-      eventSource.onmessage = e => upDateWIFIdata(JSON.parse(e.data));
-      // eventSource.onmessage =console.log  // console.log(eventSource)
-      // eventSource.onmessage = (e) => {
-      //   // console.log("e", e);
-      //   // changeRssi(JSON.parse(e.data));
-      //   // changeToggle(true);
-      // };
-
-      eventSource.onerror = () => {
-        // error log here
-
-        eventSource.close();
-      }
-      return () => {
-        eventSource.close();
-      };
-
-    }
-  }, [toggle]);
-
-  return (
-    <div className="test">
-      <TopBar/>
-      <SideBar/>
-      <button
-        onClick={() => {
-          changeToggle(!toggle);
-        }}
-      >
-        get data
-      </button>
-      {rssi}
-
-      <MainB />
-
-    </div>
-  );
+  return <div>{user ? <MainPage /> : <SignIn />}</div>;
 };
 
 export default App;
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider);
+  };
+  //style="background-color: #508bfc;"}
+  return (
+    <section className="vh-100">
+      <div className="container py-5 h-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+            <div className="card shadow-2-strong" >
+              <div className="card-body p-5 text-center">
+                <h3 className="mb-5">Sign in</h3>
+                <div className="form-outline mb-4">
+                  <input
+                    type="email"
+                    id="typeEmailX-2"
+                    className="form-control form-control-lg"
+                  />
+                  <label className="form-label" for="typeEmailX-2">
+                    Email
+                  </label>
+                </div>
+
+                <div className="form-outline mb-4">
+                  <input
+                    type="password"
+                    id="typePasswordX-2"
+                    className="form-control form-control-lg"
+                  />
+                  <label className="form-label" for="typePasswordX-2">
+                    Password
+                  </label>
+                </div>
+                <div className="form-check d-flex justify-content-start mb-4">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    value=""
+                    id="form1Example3"
+                  />
+                  <label className="form-check-label" for="form1Example3">
+                    {" "}
+                    Remember password{" "}
+                  </label>
+                </div>
+
+                <button
+                  className="btn btn-primary btn-lg btn-block"
+                  type="submit"
+                >
+                  Login
+                </button>
+
+
+
+                <button
+                  className="btn btn-lg btn-block btn-primary"
+                  // style="background-color: #dd4b39"
+                  type="submit"
+                  onClick={signInWithGoogle}
+                >
+                  <i className="fab fa-google me-2"></i> Sign in with google
+                </button>
+                <button
+                  className="btn btn-lg btn-block btn-primary mb-2"
+                  // style="background-color: #3b5998"
+                  type="submit"
+                >
+                  <i className="fab fa-facebook-f me-2"></i>Sign in with
+                  facebook
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+/* <hr className="my-4"> */
+//style="border-radius: 1rem;"
+
---
+++ client/MainPage.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import MainB from "./RSSI/MainB.jsx";
+import TopBar from "./RSSI/TopBar.jsx";
+import SideBar from "./RSSI/SideBar/SideBar.jsx";
+import SignOutComponent from "./RSSI/SignOutComponent.jsx"
+
+
+const MainPage = () => {
+  const [rssi, changeRssi] = useState(null);
+  const [toggle, changeToggle] = useState(false);
+
+  const upDateWIFIdata = (data) => {
+    console.log(data[0]);
+    changeRssi(data[0].signal_level);
+  };
+  useEffect(() => {
+    if (toggle) {
+      let eventSource = new EventSource("http://localhost:3000/rssi");
+      eventSource.onmessage = (e) => upDateWIFIdata(JSON.parse(e.data));
+      // eventSource.onmessage =console.log  // console.log(eventSource)
+      // eventSource.onmessage = (e) => {
+      //   // console.log("e", e);
+      //   // changeRssi(JSON.parse(e.data));
+      //   // changeToggle(true);
+      // };
+
+      eventSource.onerror = () => {
+        // error log here
+
+        eventSource.close();
+      };
+      return () => {
+        eventSource.close();
+      };
+    }
+  }, [toggle]);
+
+  return (
+    <div className="test">
+      <TopBar />
+      <SideBar />
+      <SignOutComponent/>
+      <button
+        onClick={() => {
+          changeToggle(!toggle);
+        }}
+      >
+        get data
+      </button>
+      {rssi}
+
+      <MainB />
+    </div>
+  );
+};
+
+
+
+export default MainPage;
+
+
